ddb92b282557d1b506133b21bbec2d2cca3f1c3c
SUDHAKAR1202 <sudzcrazy94@gmail.com>
2023-11-06 18:50:27 +0530

    first commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import LeftSidebar from '../../components/LeftSidebar/leftSidebar'
+import RightSidebar from '../../components/RightSidebar/RightSidebar'
+import HomeMainbar from "../../components/HomeMainbar/HomeMainbar";
+import '../../App.css'
+
+const Home = () => {
+  return (
+    <div className='home-container-1'>
+
+      <LeftSidebar />
+      <div className='home-container-2'>
+        <HomeMainbar />
+        <RightSidebar />
+
+      </div>
+      
+
+    </div>
+    
+  )
+}
+
+export default Home
